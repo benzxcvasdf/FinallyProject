cf5c5ae205ed48d64cc4bf7d400a4be711646140
benzxcvasdf <71902795+benzxcvasdf@users.noreply.github.com>
2024-01-25 16:43:36 +0800

    change README.md

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+from ultralytics import YOLO
+from ultralytics.utils import ASSETS
+from ultralytics.models.yolo.detect import DetectionPredictor
+import cv2
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+model = YOLO("yolov8n.pt")
+model.predict(source=0, show=True, conf=0.3)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
