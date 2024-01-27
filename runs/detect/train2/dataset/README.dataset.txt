5f77a0b1dc519e5d265887a85821d7168da16d8a
benzxcvasdf <benzxcvasdf@gmail.com>
2024-01-27 21:25:35 +0800

    changes

--- runs/detect/train2/dataset/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# Road damage tracker > 2023-07-06 11:03am
-https://universe.roboflow.com/merci/road-damage-tracker-yx8cw
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
--- train.py
+++ train.py
@@ -1,14 +1,14 @@
 
 from ultralytics import YOLO
-
+import torch
 # 加载一个模型
 model = YOLO('yolov8n.yaml')  # 从YAML建立一个新模型
 
 # 训练模型 , device=0)
 
 def run():
-    #torch.multiprocessing.freeze_support()
-    results = model.train(data='C:\\Users\\User\\PycharmProjects\\FinallyProject\\dataset\\data.yaml', epochs=100, imgsz=640, device=0)
+    torch.multiprocessing.freeze_support()
+    results = model.train(data='C:\\Users\\User\\PycharmProjects\\FinallyProject\\dataset\\archive (1)\\data.yaml', epochs=1000, device=0)
 
 if __name__ == '__main__':
     run()
