090b8612efa074b2fbc3bcfb3da79ccefabf23fa
benzxcvasdf <benzxcvasdf@gmail.com>
2024-02-07 08:10:39 +0800

    changes

--- ResumeTrain.py
+++ ResumeTrain.py
@@ -1,9 +1,16 @@
+
 from ultralytics import YOLO
+import torch
+# 加载一个模型
 
-# Load a model
-model = YOLO('C:\\Users\\User\\PycharmProjects\\FinallyProject\\runs\\detect\\train28\\weights\\last.pt')  # load a partially trained model
+model = YOLO('yolov8n.yaml')  # 从YAML建立一个新模型
+model = YOLO('C:\\Users\\User\\PycharmProjects\\FinallyProject\\runs\\detect\\train28\\weights\\last.pt')  # load a pretrained model (recommended for training)
+model = YOLO('yolov8n.yaml').load('C:\\Users\\User\\PycharmProjects\\FinallyProject\\runs\\detect\\train28\\weights\\last.pt')  # build from YAML and transfer weights
+# 训练模型 , device=0)
 
-# Resume training
-results = model.train(resume=True)
+def run():
+    torch.multiprocessing.freeze_support()
+    results = model.train(data='C:\\Users\\User\\PycharmProjects\\FinallyProject\\dataset\\archive (1)\\data.yaml', epochs=300, resume=True, device=0)
 
-git filter-branch --tree-filter 'rm -rf runs/detect/train27/weights/last.pt' HEAD
+if __name__ == '__main__':
+    run()
