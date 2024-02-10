88a5e532bb868e894d1498aeb7b751eb25a9352b
benzxcvasdf <benzxcvasdf@gmail.com>
2024-02-10 14:33:46 +0800

    changes

--- train.py
+++ train.py
@@ -2,15 +2,15 @@
 from ultralytics import YOLO
 import torch
 # 加载一个模型
-model = YOLO('yolov8n.yaml')  # 从YAML建立一个新模型
-model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
-model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
+model = YOLO('yolov8m.yaml')  # 从YAML建立一个新模型
+model = YOLO('yolov8m.pt')  # load a pretrained model (recommended for training)
+model = YOLO('yolov8m.yaml').load('yolov8m.pt')  # build from YAML and transfer weights
 
 # 训练模型 , device=0)
 
 def run():
     torch.multiprocessing.freeze_support()
-    results = model.train(data='C:\\Users\\User\\PycharmProjects\\FinallyProject\\dataset\\archive (1)\\data.yaml', epochs=150, resume=True, device=0)
+    results = model.train(data='C:\\Users\\User\\PycharmProjects\\FinallyProject\\dataset\\archive (1)\\data.yaml', epochs=1000, resume=True, device=0,batch=-1)
 
 if __name__ == '__main__':
     run()
